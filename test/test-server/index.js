c336c1ebf87b5a4d68b75d58d4ce2d94f0ce454f
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-03-16 20:52:23 +0000

    improve test server monitor logic

--- test/test-server/index.js
+++ test/test-server/index.js
@@ -100,7 +100,10 @@ const privateKey = signers[0].privateKey
     // to know when the test server is finished getting ready
     // and able to start the automated tests
     require('http')
-      .createServer((req, res) => res.end('test server ready'))
+      .createServer((req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.end('test server ready')
+      })
       .listen(59281)
   })
 })()
